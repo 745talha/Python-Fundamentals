c4e584990da31e503d2cd0550746d5cf21de0f44
Muhammad Talha Sharif <93639673+745talha@users.noreply.github.com>
2023-09-11 06:46:52 +0500

    Basic Programs
    Basic programs in python

---
+++ 6. Leap Year/Leap_year.py
@@ -0,0 +1,5 @@
+y=int(input("Enter year : "))
+if y%4==0:
+    print("Its leap year")
+else:
+    print("Its common year")
---
+++ 7. Exponent/power.py
@@ -0,0 +1,5 @@
+import math
+base = int(input("Enter base : "))
+exp = int(input("Enter exponent: "))
+double = result = math.pow(base,exp)
+print("Result : ",result)
