95b5833714e5963aa1f6e06e918827eadc09ee8b
Muhammad Talha Sharif <93639673+745talha@users.noreply.github.com>
2023-10-04 16:35:22 +0500

    Delete 11. Temperature Conversion directory

--- 11. Temperature Conversion/CelsiusToFahrenheit.py
+++
@@ -1,3 +0,0 @@
-c=float(input("Enter temperature in Celsius : "))
-float= f=(c*9/5)+32
-print(c," C = ",f," F")
--- 11. Temperature Conversion/FahrenheitToCelsius.py
+++
@@ -1,3 +0,0 @@
-f=float(input("Enter temperature in Fahrenheit : "))
-float = c=(f-32)*5/9
-print(f," F = ",c," C")
